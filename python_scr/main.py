ab7b56410331a2f93cdc38d19e54f0cb02c13207
Admin <=>
2025-12-10 03:34:09 +0200

    add new tables to database

--- python_scr/main.py
+++ python_scr/main.py
@@ -12,7 +12,7 @@ pd.set_option('display.max_colwidth', None)
 patterns_df = pd.read_csv('data/internet_usage.csv')
 patterns_df = patterns_df.replace("..", np.nan)
 
-print(patterns_df.head(10))
+# print(patterns_df.head(10))
 
 
 #################################################################### create databade for sql tools
@@ -73,9 +73,36 @@ patterns_df = patterns_df.iloc[:,[0,1,26,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17
 #print(patterns_df)
 
 
-edu_stat = pd.read_csv('data/edu.csv')
+edu_stat = pd.read_csv('data/edu.csv', na_values=['..'])
 edu_stat = edu_stat.iloc[:816,[0,1,2,4,5,6,7,8]]
 # print(edu_stat.tail(10))
+
+# engine = sql.create_engine("postgresql://postgres:*********************************")
+# metadata = sql.MetaData()
+# metadata.reflect(bind=engine)
+# if "edu" not in metadata.tables:
+#     edu = sql.Table(
+#         "edu",
+#         metadata,
+#         sql.Column("Country Name", sql.String),
+#         sql.Column("Country Code", sql.String),
+#         sql.Column("Series", sql.String),
+#         sql.Column("2000 [YR2000]", sql.Float),
+#         sql.Column("2005 [YR2005]", sql.Float),
+#         sql.Column("2010 [YR2010]", sql.Float),
+#         sql.Column("2015 [YR2015]", sql.Float),
+#         sql.Column("2020 [YR2020]", sql.Float)
+#     )
+#     metadata.create_all(engine)
+# else:
+#     edu = metadata.tables["edu"]
+#
+# with engine.begin() as conn:
+#     conn.execute(
+#         sql.insert(edu),
+#         edu_stat.to_dict(orient="records")
+#     )
+#     print("edu вставлены")
 
 edu_stat = edu_stat.melt(id_vars = ['Country Name', 'Country Code','Series'])
 edu_stat['variable'] = edu_stat['variable'].astype(str).str[:4]
@@ -84,16 +111,54 @@ edu_stat = edu_stat.rename(columns = {'variable':'year'})
 edu_stat = edu_stat.pivot(index=['Country Name','Country Code','year'],columns='Series', values='value')
 # print(edu_stat)
 
-gpd_stat = pd.read_csv('data/GDP.csv')
-# print(gpd_stat.tail(10))
-gpd_stat = gpd_stat.iloc[:798,[0,1,2,4,5,6,7,8]]
+gdp_stat = pd.read_csv('data/GDP.csv', na_values=['..'])
+gdp_stat = gdp_stat.iloc[:798,[0,1,2,4,5,6,7,8]]
 
-gpd_stat = gpd_stat.melt(id_vars = ['Country Name', 'Country Code','Series Name'])
-gpd_stat['variable'] = gpd_stat['variable'].astype(str).str[:4]
-gpd_stat['variable'] = pd.to_numeric(gpd_stat["variable"])
-gpd_stat = gpd_stat.rename(columns = {'variable':'year'})
-gpd_stat = gpd_stat.pivot(index=['Country Name','Country Code','year'],columns='Series Name', values='value')
-# print(gpd_stat)
+def normolize_gdp (row):
+    if (row['Series Name'] == 'GDP (current US$)'):
+        row['2000 [YR2000]'] = row['2000 [YR2000]']/1000000
+        row['2005 [YR2005]'] = row['2005 [YR2005]'] / 1000000
+        row['2010 [YR2010]'] = row['2010 [YR2010]'] / 1000000
+        row['2015 [YR2015]'] = row['2015 [YR2015]'] / 1000000
+        row['2020 [YR2020]'] = row['2020 [YR2020]'] / 1000000
+    return row
+gdp_stat = gdp_stat.apply(normolize_gdp, axis='columns')
+# print(gdp_stat.head(10))
+
+# engine = sql.create_engine("postgresql://postgres:*********************************")
+# metadata = sql.MetaData()
+# metadata.reflect(bind=engine)
+# if "gdp" not in metadata.tables:
+#     gdp = sql.Table(
+#         "gdp",
+#         metadata,
+#         sql.Column("Country Name", sql.String),
+#         sql.Column("Country Code", sql.String),
+#         sql.Column("Series", sql.String),
+#         sql.Column("2000 [YR2000]", sql.Float),
+#         sql.Column("2005 [YR2005]", sql.Float),
+#         sql.Column("2010 [YR2010]", sql.Float),
+#         sql.Column("2015 [YR2015]", sql.Float),
+#         sql.Column("2020 [YR2020]", sql.Float)
+#     )
+#     metadata.create_all(engine)
+# else:
+#     gdp = metadata.tables["gdp"]
+#
+# with engine.begin() as conn:
+#     conn.execute(
+#         sql.insert(gdp),
+#         gdp_stat.to_dict(orient="records")
+#     )
+#     print("gdp вставлены")
+
+
+gdp_stat = gdp_stat.melt(id_vars = ['Country Name', 'Country Code','Series Name'])
+gdp_stat['variable'] = gdp_stat['variable'].astype(str).str[:4]
+gdp_stat['variable'] = pd.to_numeric(gdp_stat["variable"])
+gdp_stat = gdp_stat.rename(columns = {'variable':'year'})
+gdp_stat = gdp_stat.pivot(index=['Country Name','Country Code','year'],columns='Series Name', values='value')
+print(gdp_stat)
 
 ########################################################### EDA & Analisys
 
